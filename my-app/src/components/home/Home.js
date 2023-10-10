9003865262007fe08695543822fe79437cfb6f25
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-10-10 09:55:44 +0200

    mobile one item design on home

--- my-app/src/components/home/Home.js
+++ my-app/src/components/home/Home.js
@@ -6,18 +6,28 @@ import classes from './Home.module.css';
 
 const Home = ({ products: productsData }) => {
 	const products = productsData.map(product => {
+		const { name, imageUrl, imagesAlts, annotation, price, discount, description } = product;
+		const totalPrice = (price * ((100 - discount) / 100)).toFixed(2);
+
 		return (
 			<li key={product.name} className={classes.li}>
-				<img src={product.imageUrl} alt={product.imagesAlts.image1} className={classes.img} />
-				<section className={classes.details}>
-					<p className={classes.annotation}>{product.annotation}</p>
-					<Link to="/products/product-1" className={classes.name}>
-						{product.name}
-					</Link>
-					<p className={classes.price}>{product.price}</p>
-					<span className={classes.discount}>{product.discount}%</span>
-					<p className={classes.description}>{product.description}</p>
-				</section>
+				<Link to="/products/product-1">
+					<img src={imageUrl} alt={imagesAlts.image1} className={classes.img} />
+					<section className={classes.details}>
+						<p className={classes.annotation}>{annotation}</p>
+						<p className={classes.name}>{name}</p>
+						<p className={classes.price}>
+							<span className={classes['discounted-price']}>
+								${totalPrice}
+								{Number.isInteger(totalPrice) && '.00'}
+							</span>
+							<span className={classes['original-price']}>${price}</span>
+							<span className={classes['discount-percent']}>{discount}%</span>
+						</p>
+
+						<p className={classes.description}>{description}</p>
+					</section>
+				</Link>
 			</li>
 		);
 	});
@@ -25,7 +35,7 @@ const Home = ({ products: productsData }) => {
 	return (
 		<Wrapper>
 			<main className={classes.main}>
-				<h1>Feel free to browse our assortment!</h1>
+				<h1 className={classes.h1}>sneakers</h1>
 				<ul className={classes.ul}>{products}</ul>
 			</main>
 		</Wrapper>
