cba605965beacd92df5b1651ecd063ffc933546f
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-10-10 14:31:30 +0200

    made thumbnails classes dynamic

--- my-app/src/components/product/ImagesThumbnails.js
+++ my-app/src/components/product/ImagesThumbnails.js
@@ -2,17 +2,24 @@ import { useState } from 'react';
 
 import classes from './ImagesThumbnails.module.css';
 
-const initialThumbnailClasses = {
-	0: `${classes.img} `,
-	2: `${classes.img} `,
-	4: `${classes.img} `,
-	6: `${classes.img}`,
+const initializeThumbnailClasses = urls => {
+	const initialThumbnailClasses = {};
+
+	urls.forEach((url, index) => {
+		if (index % 2 === 0) {
+			initialThumbnailClasses[index] = `${classes.img}`;
+		}
+	});
+
+	return initialThumbnailClasses;
 };
 
 const ImagesThubmnails = ({ urls, alts, setActualImageIndex }) => {
+	const initialThumbnailClasses = initializeThumbnailClasses(urls);
+
 	const [thumbnailsClasses, setThumbnailsClasses] = useState({
 		...initialThumbnailClasses,
-		1: `${classes.img} ${classes['img-active']}`,
+		0: `${classes.img} ${classes['img-active']}`,
 	});
 
 	const imageIndexDesktopHandler = index => {
@@ -25,21 +32,26 @@ const ImagesThubmnails = ({ urls, alts, setActualImageIndex }) => {
 			return newState;
 		});
 	};
+
 	const thumbnails = [];
 
-	for (let i = 0; i < urls.length; i += 2) {
+	for (let i = 0, j = 1; i < urls.length; i += 2, j++) {
+		const altIndex = `image${j}`;
+
 		thumbnails.push(
-			<button
-				type="button"
-				className={`${classes['img-btn']}`}
-				aria-label="thumbnail image 1"
-				onClick={() => imageIndexDesktopHandler(i)}>
-				<img src={urls[i]} alt={alts.image1} className={thumbnailsClasses[i]} />
-			</button>,
+			<li key={urls[i]}>
+				<button
+					type="button"
+					className={`${classes['img-btn']}`}
+					aria-label={`thumbnail ${altIndex}`}
+					onClick={() => imageIndexDesktopHandler(i)}>
+					<img src={urls[i]} alt={alts[altIndex]} className={thumbnailsClasses[i]} />
+				</button>
+			</li>,
 		);
 	}
 
-	return <div className={classes['thumbnails']}>{thumbnails}</div>;
+	return <ul className={classes['thumbnails']}>{thumbnails}</ul>;
 };
 
 export default ImagesThubmnails;
