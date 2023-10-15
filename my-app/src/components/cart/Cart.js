9fb584014357838973a16d5e00d5ba1fb71bdab0
Patryk Sąsiadek <patrykskontakt@gmail.com>
2023-10-15 22:30:47 +0200

    cart items styling

--- my-app/src/components/cart/Cart.js
+++ my-app/src/components/cart/Cart.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 
 import classes from './Cart.module.css';
+import deleteIcon from '../../assets/images/icon-delete.svg';
 
 const Cart = () => {
 	let content;
@@ -9,30 +10,36 @@ const Cart = () => {
 	if (items.length > 0) {
 		const mappedItems = items.map(item => {
 			return (
-				<li key={item.id}>
-					<img src="" className={classes.img} alt="item" />
-					<h2 className={classes.name}>{item.name}</h2>
+				<li key={item.id} className={classes.item}>
+					<img src={item.imageUrl} className={classes['item-img']} alt="item" />
+
+					<h3 className={classes.name}>{item.name}</h3>
 					<p className={classes.pricing}>
-						{`$${item.price} x ${item.quantity}`}
+						{`$${item.price} x ${item.quantity} `}
 						<span className={classes['total-price']}>{`$${item.price * item.quantity}`}</span>
 					</p>
+
 					<button className={classes['remove-item-btn']}>
-						<img />
+						<img src={deleteIcon} alt="delete " />
 					</button>
 				</li>
 			);
 		});
-		content = mappedItems;
+		content = <ul className={classes.list}>{mappedItems}</ul>;
 	} else {
 		content = (
 			<>
-				<h2 className={classes.heading}>Cart</h2>
 				<div className={classes.items}></div>
 				<p className={classes.empty}>Your cart is empty.</p>
 			</>
 		);
 	}
-	return <section className={classes.cart}>{content}</section>;
+	return (
+		<section className={classes.cart}>
+			<h2 className={classes.heading}>Cart</h2>
+			{content}
+		</section>
+	);
 };
 
 export default Cart;
