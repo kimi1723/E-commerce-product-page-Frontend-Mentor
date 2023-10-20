a116be9ff7d5272ae5bc237f9f89ad4400758802
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-10-20 16:58:53 +0200

    savings logic

--- my-app/src/components/checkout/Summary.js
+++ my-app/src/components/checkout/Summary.js
@@ -10,7 +10,7 @@ const Summary = () => {
 	const totalQuantity = useSelector(state => state.cart.totalQuantity);
 	const dispatch = useDispatch();
 
-	const selectOptions = Array.from({ length: 10 }, (e, i) => ({ value: i + 1, label: i + 1 }));
+	const selectOptions = Array.from({ length: 11 }, (e, i) => ({ value: i, label: i }));
 
 	const products = cart.map(product => {
 		const { id, imageUrl, alt, annotation, name, originalPrice, discountedPrice, quantity } = product;
@@ -32,6 +32,7 @@ const Summary = () => {
 				...styles,
 				boxShadow: state.isFocused ? '0px 0px 2px 1px hsl(26, 100%, 55%)' : 'none',
 				border: state.isFocused ? '1px solid hsl(26, 100%, 55%)' : '1px solid hsl(220, 13%, 13%)',
+				transition: 'border 0.2s, box-shadow 0.2s',
 				cursor: 'pointer',
 				'&:hover': { border: '1px solid hsl(26, 100%, 55%)' },
 			}),
@@ -62,6 +63,15 @@ const Summary = () => {
 		);
 	});
 
+	const calculateSavings = () => {
+		const originalTotal = cart.map(product => product.originalPrice * product.quantity).reduce((a, b) => a + b, 0);
+		const discountedTotal = cart.map(product => product.discountedPrice * product.quantity).reduce((a, b) => a + b, 0);
+
+		const savings = (originalTotal - discountedTotal).toFixed(2);
+
+		return savings;
+	};
+
 	return (
 		<>
 			<header className={classes.header}>
@@ -72,6 +82,7 @@ const Summary = () => {
 				<ul className={classes.list} role="list">
 					{products}
 				</ul>
+				<p>You are saving ${calculateSavings()} </p>
 			</main>
 		</>
 	);
