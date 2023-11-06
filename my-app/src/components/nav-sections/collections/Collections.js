29d52cbe4f4fb25e9679c3fc21f3cd7cdb4559b9
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-11-06 16:25:21 +0100

    variants

--- my-app/src/components/nav-sections/collections/Collections.js
+++ my-app/src/components/nav-sections/collections/Collections.js
@@ -6,16 +6,24 @@ import fall from '../../../assets/images/collections/fall.jpg';
 import spring from '../../../assets/images/collections/spring.jpg';
 
 const Collections = () => {
-	
+	const variants = {
+		initial: { scale: 0.5 },
+		animate: { scale: 1 },
+		hover: { scale: 1.05 },
+		transition: { duration: 1, type: 'spring', bounce: 0.25 },
+	};
+
 	return (
 		<main className={classes.main}>
 			<h1 className={classes.title}>Collections</h1>
 			<section className={classes.collections}>
 				<Link className={classes.link} to="fall">
 					<motion.div
-						initial={{ scale: 0.5 }}
-						animate={{ scale: 1 }}
-						transition={{ duration: 1, type: 'spring', bounce: 0.25 }}>
+						variants={variants}
+						initial="initial"
+						animate="animate"
+						whileHover="hover"
+						transition="transition">
 						<div className={classes['hero-bg']}></div>
 						<p className={classes.name}>Fall</p>
 						<img src={fall} className={classes.img} alt="" />
@@ -24,9 +32,11 @@ const Collections = () => {
 
 				<Link className={classes.link} to="spring">
 					<motion.div
-						initial={{ scale: 0.5 }}
-						animate={{ scale: 1 }}
-						transition={{ duration: 1, type: 'spring', bounce: 0.25 }}>
+						variants={variants}
+						initial="initial"
+						animate="animate"
+						whileHover="hover"
+						transition="transition">
 						<div className={classes['hero-bg']}></div>
 						<p className={classes.name}>Spring</p>
 						<img src={spring} className={classes.img} alt="" />
