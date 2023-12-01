648d7f773d13da543e97bf21259ec5d29fb1a8c7
Patryk Sąsiadek <patrykskontakt@gmail.com>
2023-12-01 03:55:05 +0100

    .env v2

--- src/firebaseInitialization.js
+++ src/firebaseInitialization.js
@@ -3,13 +3,13 @@ import { getStorage } from 'firebase/storage';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
+	appId: process.env.appId,
 	apiKey: process.env.apiKey,
 	authDomain: process.env.authDomain,
 	databaseURL: process.env.databaseURL,
 	projectId: process.env.projectId,
 	storageBucket: process.env.storageBucket,
 	messagingSenderId: process.env.messagingSenderId,
-	appId: process.env.appId,
 };
 
 const app = initializeApp(firebaseConfig);
