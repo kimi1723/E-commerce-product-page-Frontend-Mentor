630f90de7cfb2de3a424bb31ba50e5e029214a8d
Patryk Sąsiadek <patrykskontakt@gmail.com>
2023-12-01 04:11:19 +0100

    playing with .env

--- src/firebaseInitialization.js
+++ src/firebaseInitialization.js
@@ -2,14 +2,22 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getDatabase } from 'firebase/database';
 
+const apiKey = process.env.REACT_APP_API_KEY;
+const authDomain = process.env.REACT_APP_AUTH_DOMAIN;
+const databaseURL = process.env.REACT_APP_DATABASE_URL;
+const projectId = process.env.REACT_APP_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_APP_ID;
+
 const firebaseConfig = {
-	appId: process.env.appId,
-	apiKey: process.env.apiKey,
-	authDomain: process.env.authDomain,
-	databaseURL: process.env.databaseURL,
-	projectId: process.env.projectId,
-	storageBucket: process.env.storageBucket,
-	messagingSenderId: process.env.messagingSenderId,
+	apiKey,
+	authDomain,
+	databaseURL,
+	projectId,
+	storageBucket,
+	messagingSenderId,
+	appId,
 };
 
 const app = initializeApp(firebaseConfig);
