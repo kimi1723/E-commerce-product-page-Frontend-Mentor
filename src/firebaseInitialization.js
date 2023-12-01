37017b26320c5700b8f29137303730a9251db150
Patryk Sąsiadek <patrykskontakt@gmail.com>
2023-12-01 01:56:33 +0100

    lifted folder, name change

--- src/firebaseInitialization.js
+++ src/firebaseInitialization.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getDatabase } from 'firebase/database';
-import firebaseConfig from './firebaseConfig';
+import firebaseConfig from './firebase-config';
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
